f33e5b8b8b8c337ea757bcb601bbe4ba177b001e
Daniela GF <70969163+Danielagf02@users.noreply.github.com>
2022-12-04 00:34:22 -0600

    interfaces listas

---
+++ app/src/main/java/com/example/clinicadental/crearcuenta.java
@@ -0,0 +1,17 @@
+package com.example.clinicadental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class crearcuenta extends AppCompatActivity {
+    Button btnCrearCuenta;
+    Button btnSubirFoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crearcuenta);
+    }
+}
