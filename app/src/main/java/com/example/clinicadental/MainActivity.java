8a18291ac42c040970adb3ecb9286e283def7008
Yamile <57511759+Yamile0504@users.noreply.github.com>
2022-12-04 14:42:09 -0600

    corrigieron errores de import

--- app/src/main/java/com/example/clinicadental/MainActivity.java
+++ app/src/main/java/com/example/clinicadental/MainActivity.java
@@ -1,19 +1,17 @@
 package com.example.clinicadental;
-
 import static com.example.clinicadental.LoginActivity.tiporecuperado;
-
 import android.os.Bundle;
 import android.view.Menu;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import com.example.clinicadental.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
+import com.example.clinicadental.databinding.ActivityMainBinding;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
